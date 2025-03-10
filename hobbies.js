006a712f0acda0a07cef2225a525116585cbd475
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-10 15:15:43 +0100

    add second function

---
+++ hobbies.js
@@ -0,0 +1,5 @@
+function hobby(hobbyOne, hobbyTwo, hobbyThree){
+    return [hobbyOne, hobbyTwo, hobbyThree]
+}
+
+module.exports = hobby
--- server.js
+++ server.js
@@ -1,2 +1,5 @@
 const yourName = require("./names.js")  
 console.log(yourName("Vincenzo", "Terracciano"));
+
+const hobby = require("./hobbies.js")
+console.log(hobby("calcio", "musica", "palestra"));
